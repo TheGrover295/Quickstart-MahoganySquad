9392a1efbe8f3b3f71585acbb44c2366f173d91c
TheGrover295 <william.bertolini@tutamail.com>
2025-10-20 16:34:16 -0500

    added mass for the robot and centrpical force. change latter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -11,13 +11,15 @@ import org.slf4j.LoggerFactory;
 
 public class Constants {
     private static final Logger log = LoggerFactory.getLogger(Constants.class);
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5); // change;
+
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
-                .pathConstraints(pathConstraints) // William test
+                .pathConstraints(pathConstraints)
                 .build(); //super cool cool
 
     }
