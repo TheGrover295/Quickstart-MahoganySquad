891604233c58fbfc42ef79ac6c326ab2f104bf94
TheGrover295 <william.bertolini@tutamail.com>
2025-11-29 10:54:27 -0600

    made a test auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/AutoTestV1.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.Auto;
+
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.util.Timer;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous
+public class AutoTestV1 extends OpMode {
+    private Follower follower;
+
+    private Timer pathTimer, opModeTimer;
+
+    public enum PathState {
+        // START POSITION_END POSITION
+        // DRIVE > MOVEMENT STATE
+        // SHOOT > ATTEMPT TO SCORE
+
+        DRIVE_STARTPOS_SHOOTPOS,
+        SHOOT_PRELOAD
+    }
+
+    PathState pathState;
+
+    private final Pose startPose = new Pose(20.629213483146067, 122.15730337078651, Math.toRadians(138));
+    private final Pose shootPose = new Pose(48.13483146067416, 95.6629213483146, Math.toRadians(138));
+
+    private PathChain driveStartPosShootPos; // single path
+
+    public void buildPaths(){
+        //put in coordinates for startgin pose > ending pose
+        driveStartPosShootPos = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, shootPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), shootPose.getHeading())
+                .build();
+    }
+
+    public void StatePathUpdate(){
+        switch(pathState) {
+            case DRIVE_STARTPOS_SHOOTPOS:
+                follower.followPath(driveStartPosShootPos, true);
+                setPathState(PathState.SHOOT_PRELOAD); // reset timer and make new state
+                break;
+            case SHOOT_PRELOAD:
+
+                //check is follower  done its path?
+                if (!follower.isBusy()) {
+                    //TODO add logic to flyweel shooter
+                    telemetry.addLine("Done Path 1");
+                    // transition to next state
+                }
+                break;
+            default:
+                telemetry.addLine("No State Commanded");
+                break;
+        }
+    }
+
+    public void setPathState(PathState newState){
+        pathState = newState;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void init() {
+        pathState = PathState.DRIVE_STARTPOS_SHOOTPOS;
+        pathTimer = new Timer();
+        opModeTimer = new Timer();
+        follower = Constants.createFollower(hardwareMap);
+        //TODO add in any other init mechanisms
+
+        buildPaths();
+        follower.setPose(startPose);
+    }
+
+    public void start(){
+        opModeTimer.resetTimer();
+        setPathState(pathState);
+    }
+
+
+    @Override
+    public void loop() {
+        follower.update();
+        StatePathUpdate();
+
+        telemetry.addData("path state", pathState.toString());
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.addData("path time", pathTimer.getElapsedTimeSeconds());
+    }
+}
