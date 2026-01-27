ecdf421c38942c94ff8501eb3d991bfab7886891
TheGrover295 <william.bertolini@tutamail.com>
2026-01-26 20:01:15 -0600

    Auto Things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/AutonomousModeClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/AutonomousModeClose.java
@@ -13,7 +13,7 @@ import com.qualcomm.robotcore.util.RobotLog;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.pedroPathing.mechanisms.Limelight;
 import org.firstinspires.ftc.teamcode.pedroPathing.vision.GoalTargeter;
-import org.firstinspires.ftc.teamcode.pedroPathing.vision.MotifDetector;
+import org.firstinspires.ftc.teamcode.pedroPathing.vision.MotifDetector; //cool
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
